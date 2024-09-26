c3d4cc6ce6715ad141de6ebb21243cbfb5f07a84
KricsDavid <david@kricsfalusi.hu>
2024-09-26 11:51:43 +0200

    ghuzg

--- Valasztas/Models/Partt.cs
+++ Valasztas/Models/Partt.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Valasztas.Models
 {
     public class Part
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Key]
 
         public string rovidnev { get; set; }
---
+++ Valasztas/Pages/Eredmeny.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Valasztas.Pages
+{
+    public class EredmenyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
